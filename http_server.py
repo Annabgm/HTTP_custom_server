80a958bb3728a1641b9b712913f395de5234e991
AnyaB <aubogomolova>
2021-07-31 01:41:02 +0300

    url encoding fix and bug fix

--- http_server.py
+++ http_server.py
@@ -5,6 +5,7 @@ from optparse import OptionParser
 import logging
 import os
 import sys
+import re
 
 
 class HTTPSever:
@@ -74,7 +75,7 @@ class HTTPSever:
                 req_file = 'index.html'
             elif req_file[-1] == '/':
                 req_file = ''.join([req_file, 'index.html'])
-            logging.info("Request file to response is{}\n".format(req_file))
+            logging.info("Request file to response is {}\n".format(req_file))
             response = self.handler(method, req_file)
             client.sendall(response)
         client.close()
@@ -85,11 +86,11 @@ class HTTPHandler:
         self._dir = dir
 
     def __call__(self, method, file_name):
-        server = 'Server: OTUServer\n'
-        date = 'Date: {}\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        connection = 'Connection: keep-alive\n\n'
+        server = 'Server: OTUServer\r\n'
+        date = 'Date: {}\r\n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        connection = 'Connection: keep-alive\r\n\r\n'
         if method == 'GET':
-            header, response = self.do_GET(file_name)
+            header, response = self.do_GET(self.urldecode(file_name))
         elif method == 'HEAD':
             header = self.do_HEAD(file_name)
             response = ''.encode('utf-8')
@@ -101,33 +102,49 @@ class HTTPHandler:
         final_response = header + const_response + response
         return final_response
 
+    def urldecode(self, file_name):
+        p = re.compile(r'\%\w{2}')
+        symbols = p.split(file_name)
+        special = p.findall(file_name)
+        name = [symbols[0]]
+        for i in range(len(symbols[1:])):
+            name.append(bytearray.fromhex(special[i].lstrip('%')).decode())
+            name.append(symbols[i+1])
+        return ''.join(name)
+
+
     def do_GET(self, file_name):
         try:
             with open(os.path.join(self._dir, file_name), 'rb') as f:
                 response = f.read()
-            status = 'HTTP/1.1 200 OK\n'
-            content_type = 'Content-Type: {}\n'.format(self.define_type(file_name))
-            content_length = 'Content-Length: {}\n'.format(len(response))
+            status = 'HTTP/1.1 200 OK\r\n'
+            content_type = 'Content-Type: {}\r\n'.format(self.define_type(file_name))
+            content_length = 'Content-Length: {}\r\n'.format(len(response))
             header = ''.join([status, content_type, content_length])
+            logging.info("Response body is {}\n".format(response))
         except FileNotFoundError:
-            header = 'HTTP/1.1 404 Not Found\n'
+            header = 'HTTP/1.1 404 Not Found\r\n'
             response = '<html><body><center><h3>Error 404: File not found</h3></center></body></html>'.encode(
                 'utf-8')
         except PermissionError:
-            header = 'HTTP/1.1 403 Forbidden\n'
+            header = 'HTTP/1.1 403 Forbidden\r\n'
             response = '<html><body><center><h3>Error 403: Access forbidden</h3></center></body></html>'.encode(
                 'utf-8')
+        logging.info("Response header is {}\n".format(header))
         header = header.encode('utf-8')
         return header, response
 
     def do_HEAD(self, file_name):
-        if os.path.exists(os.path.join(self._dir, file_name)):
-            status = 'HTTP/1.1 200 OK\n'
-            content_type = 'Content-Type: {}\n'.format(self.define_type(file_name))
-            content_length = 'Content-Length: 0\n'
+        try:
+            with open(os.path.join(self._dir, file_name), 'rb') as f:
+                response = f.read()
+            status = 'HTTP/1.1 200 OK\r\n'
+            content_type = 'Content-Type: {}\r\n'.format(self.define_type(file_name))
+            content_length = 'Content-Length: {}\r\n'.format(len(response))
             header = ''.join([status, content_type, content_length])
-        else:
+        except:
             header = 'HTTP/1.1 404 Not Found\n'
+        logging.info("Response header is {}\n".format(header))
         header = header.encode('utf-8')
         return header
 
@@ -147,7 +164,7 @@ class HTTPHandler:
         elif file_name.endswith(".swf"):
             mimetype = 'application/x-shockwave-flash'
         else:
-            raise ValueError('Unsupported format file error.')
+            mimetype = 'text/plain'
         return mimetype
 
 
